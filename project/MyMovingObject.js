17d7747ae5d96a8c7f20a3f84925c9bcc91f9a27
Francisco Pires <up201908044@fe.up.pt>
2021-04-20 17:53:29 +0100

    proj-1

---
+++ project/MyMovingObject.js
@@ -0,0 +1,94 @@
+import {CGFappearance, CGFobject} from '../lib/CGF.js';
+import {MyTriangle} from "./MyTriangle.js";
+import {MyQuad} from "./MyQuad.js";
+/**
+ * MyDiamond
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyMovingObject extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initMaterials();
+		//angle around YY with 0 in z+
+		this.orientation = 0.0;
+		this.velocity = [0.0,0.0,0.0];
+		this.position = [0.0,0.0,0.0]; 
+		this.tri0 = new MyTriangle(scene);
+		this.tri1 = new MyTriangle(scene);
+		this.tri2 = new MyTriangle(scene);
+		this.tri3 = new MyTriangle(scene);
+		this.quad = new MyQuad(scene);
+	}
+	
+	initMaterials(){
+		this.material = new CGFappearance(this.scene);
+        this.material.setAmbient(1.0, 1.0, 1.0, 1);
+        this.material.setDiffuse(1.0, 1.0, 1.0, 1);
+        this.material.setSpecular(1.0, 1.0, 1.0, 1);
+        this.material.setShininess(10.0);
+	}
+
+	turn(val){
+		this.orientation += val;
+	}
+
+	accelerate(val){
+		this.velocity[2] += val;
+	}
+
+	reset(){
+		this.position = [0,0,0];
+		this.velocity = [0,0,0];
+		this.orientation = 0;
+	}
+
+	update(){
+		var cos = Math.cos(this.orientation);
+		var sin = Math.sin(this.orientation);
+		/**var rotation = [
+						cos,	0,	-sin,	0,
+						0,		1,	0,		0,
+						sin,	0,	cos,	0,
+						0,		0,	0,		1
+					];*/
+		this.position[0] += this.velocity[0]*cos + this.velocity[2]*sin;
+		this.position[1] += this.velocity[1];
+		this.position[2] += this.velocity[2]*cos - this.velocity[0]*sin;
+		// this.pos += rotation * this.velocity;
+	}
+
+	display(){
+		this.scene.pushMatrix();
+		this.scene.translate(this.position[0],this.position[1], this.position[2]);
+		this.scene.rotate(this.orientation,0,1,0);
+
+			this.scene.pushMatrix();
+			this.scene.translate(0,0,-0.5);
+			this.scene.rotate(Math.PI,0,1,0);
+			this.quad.display();
+			this.scene.popMatrix();
+
+			this.scene.pushMatrix();
+			this.tri0.display();
+			this.scene.popMatrix();
+
+			this.scene.pushMatrix();
+			this.scene.rotate(Math.PI/2,0,0,1);
+			this.tri1.display();
+			this.scene.popMatrix();
+
+			this.scene.pushMatrix();
+			this.scene.rotate(Math.PI,0,0,1);
+			this.tri2.display();
+			this.scene.popMatrix();
+
+			this.scene.pushMatrix();
+			this.scene.rotate(-Math.PI/2,0,0,1);
+			this.tri3.display();
+			this.scene.popMatrix();
+
+		this.scene.popMatrix();
+	}
+}
+
